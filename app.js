0d9c06d8149505472582e634457aab86b01ab488
identumu <identumu@gmail.com>
2020-04-20 07:34:26 -0400

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ h1.innerHTML = "This text is inserted with Javascript"; //use the element to wri
 const input = document.getElementById("inp"); //grab elements from the dom
 
 input.value = "hello"; //use the element to write to the browser
+
+function add() {
+  const add = document.getElementById("add");
+  
+  add.addEventListener('click');
+  
+  const sum = 2 + 2;
+  
+  console.log(sum);
+}
+
+add.onclick = add();
