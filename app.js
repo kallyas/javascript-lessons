750cf0b0a7ae0a465d1b39914acc32bf6aa4825b
identumu <identumu@gmail.com>
2020-04-20 07:26:39 -0400

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const h1 = document.getElementById("header");
+const h1 = document.getElementById("header"); //grab elements from the dom
 
-h1.innerHTML = "This text is inserted with Javascript";
+h1.innerHTML = "This text is inserted with Javascript"; //use the element to write to the browser
+
+const input = document.getElementById("inp"); //grab elements from the dom
+
+input.value = "hello"; //use the element to write to the browser
